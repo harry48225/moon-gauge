c49f5470f0bbd53b2b6d2e55315a7f7e25811aa0
Harry <harrybest800@gmail.com>
2021-03-28 16:34:39 +0100

    ticking clock voltmeter

--- code.py
+++ code.py
@@ -3,6 +3,21 @@ import adafruit_ds3231
 import time
 
 import board
+import pwmio
+
+class Voltmeter_Display(object):
+
+    def __init__(self, pin):
+        ''' pin should be a pwmio.PWMOut '''
+        self.pin = pwmio.PWMOut(pin, frequency = 1024, duty_cycle = 0)
+        self.PWM_MAX = 65535
+
+
+    def display_percentage(self, percentage):
+        ''' percentage should be 0-1 '''
+        self.pin.duty_cycle = int(percentage * self.PWM_MAX)
+        print(self.pin.duty_cycle)
+
 
 SCL = board.GP17
 SDA = board.GP16
@@ -16,6 +31,16 @@ if False:
 
     rtc.datetime = t
 
+
+# main loop
+
+display = Voltmeter_Display(board.GP1)
+
+print("started")
 while True:
     print(rtc.datetime)
-    time.sleep(10)
+
+    display.display_percentage(rtc.datetime.tm_sec/60)
+
+
+    time.sleep(0.1)
